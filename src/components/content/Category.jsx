be538b19a5bcdd5105eec74feb825cd564a0d52b
ParitaSriwasut <parita.sri0327@gmail.com>
2023-12-25 02:13:39 +0700

    frontend is successed with deployment progess

--- src/components/content/Category.jsx
+++ src/components/content/Category.jsx
@@ -13,7 +13,7 @@ export default function Category() {
         <div className="2xl:mx-auto 2xl:container py-12 px-4 sm:px-6 xl:px-20 2xl:px-0 w-full">
           <div className="flex flex-col justify-center items-center space-y-10">
             <div className="flex flex-col justify-center items-center">
-              <h1 className="text-4xl font-semibold leading-7 xl:leading-9 text-neutral border border-neutral border-4 p-8 ">
+              <h1 className="text-3xl font-semibold leading-7 xl:leading-9 text-neutral border-4 border-neutral p-8 ">
                 Shop By Category
               </h1>
             </div>
@@ -39,7 +39,7 @@ export default function Category() {
                     src={myKids}
                     alt="kids-image"
                   />
-                  <button className="bg-neutral text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 bottom-4 z-10   text-xl text-bold leading-none py-3 w-36 mt-5">
+                  <button className="bg-neutral text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 bottom-4 z-10 text-xl text-bold leading-none py-3 w-36 mt-5">
                     Kids
                   </button>
                 </div>
--- src/components/header/HomeHeader.jsx
+++ src/components/header/HomeHeader.jsx
@@ -7,21 +7,20 @@ export default function HomeHeader() {
         alt="hero"
         src={myHero}
       />
-      <div className="absolute inset-0 flex items-center justify-start mt-96 px-36">
+      <div className="absolute inset-0 flex items-center justify-start mt-96 px-36 ">
         <div className="text-start justify-start text-white">
-          <h1 className="text-7xl font-semibold font-serif text-left">
+          <h1 className="text-5xl font-semibold font-serif text-left">
             Care For Your Skin,
           </h1>
-          <h1 className="text-7xl font-semibold font-serif text-left">
+          <h1 className="text-5xl font-semibold font-serif text-left pb-4">
             Care For Your Beauty
           </h1>
-          <h1 className="flex font-mono text-3xl text-left">
+          <h1 className="flex font-mono text-2xl text-left">
             What is your skin trying to tell you? Often the skin is a metaphor
             for deeper issues and a way for your body to send up a red flag to
-            warn you that all is not well underneath.
-          </h1>
-          <h1 className="flex font-mono text-3xl text-left">
-            taking care of your skin is more important than covering it up.
+            warn you that all is not well underneath. taking care of your skin
+            is more important than covering it up. So Let's “Eat, sleep,
+            skincare, repeat.”
           </h1>
         </div>
       </div>
