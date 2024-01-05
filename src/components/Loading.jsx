1a624b3a96ac7aa5decfa992cbd964c4d8a383b5
ParitaSriwasut <parita.sri0327@gmail.com>
2024-01-05 13:24:43 +0700

    st Loading compponent

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,11 @@
 export default function Loading() {
   return (
-    <div className="flex h-screen items-center justify-center bg-white">
-      <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div>
+    <div className="flex justify-center items-center h-screen">
+      <div className="relative inline-flex">
+        <div className="w-8 h-8 bg-sky rounded-full"></div>
+        <div className="w-8 h-8 bg-sky rounded-full absolute top-0 left-0 animate-ping"></div>
+        <div className="w-8 h-8 bg-sky rounded-full absolute top-0 left-0 animate-pulse"></div>
+      </div>
     </div>
   );
 }
