4f6e95e424f3cd7d04e1d3471a4a8bb3ae609f3b
ParitaSriwasut <parita.sri0327@gmail.com>
2024-01-16 22:02:00 +0700

    check reDirectIfadmin was forcing admin to Admin/product

--- src/features/auth/RedirectIfAdmin.jsx
+++ src/features/auth/RedirectIfAdmin.jsx
@@ -2,10 +2,12 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../../hooks/use-auth";
 
 export default function RedirectIfAdmin({ children }) {
-  const { authUser } = useAuth();
+  // It's not required following code was always redirect to admin/product : not sure to delete just keeping this code to avoid might be use in next project
 
-  if (authUser) {
-    return <Navigate to="/admin/product" />;
-  }
+  // const { authUser } = useAuth();
+
+  // if (authUser) {
+  //   return <Navigate to="/admin/product" />;
+  // }
   return children;
 }
